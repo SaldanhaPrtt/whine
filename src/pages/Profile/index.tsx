53bc415faed2e366526eb28f05e39afeb6c6b479
Antonio Saldanha <antonio.pretto@hotmail.com>
2023-09-12 15:04:13 -0300

    new jsx icon for colorbar

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -4,10 +4,12 @@ import SquareButton from '../../components/ProductButton';
 import styles from './styles';
 import Header from '../../components/Header';
 import { Button } from 'react-native-paper';
+import WineGlassIcon from '../../assets/JSXAssets/WineGlassIcon';
 
 export default function Profile({navigation, route} : any) {
   return (
     <View style={styles.container}>
+      <WineGlassIcon />
       <Button mode='contained-tonal' onPress={() => navigation.navigate('RegisterWhine')}>Registrar Vinho</Button>
     </View>
   );
