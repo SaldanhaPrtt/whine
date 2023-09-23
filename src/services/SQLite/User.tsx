45f9c8076ec797b37e53415617cf9f76c53ba613
Antonio Saldanha <antonio.pretto@hotmail.com>
2023-09-23 18:53:27 -0300

    Users table ToDo

---
+++ src/services/SQLite/User.tsx
@@ -0,0 +1,23 @@
+// import { runTransactionQuery } from './SQLite';
+
+// type User = {
+
+// }
+
+// async function createWinesTable(db: any) {
+//     await runTransactionQuery(
+//       db,
+//       `CREATE TABLE IF NOT EXISTS wines (
+//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         name TEXT NOT NULL,
+//         price FLOAT NOT NULL,
+//         oldPrice FLOAT,
+//         year INTEGER NOT NULL,
+//         grapes TEXT NOT NULL,
+//         country TEXT NOT NULL,
+//         region TEXT NOT NULL,
+//         description TEXT NOT NULL,
+//         image TEXT NOT NULL
+//       );`,
+//     );
+//   }
