549770c5f0b8a98045c6646d725a8e3b2fdb9a0d
Antonio Saldanha <antonio.pretto@hotmail.com>
2023-09-28 14:11:10 -0300

    Dynamic cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -6,7 +6,7 @@ import MinusIcon from '../../assets/JSXAssets/MinusIcon';
 import wine_png from '../../assets/PNG/wine_png.png';
 import { useCart } from '../../contexts/cart';
 
-const CartItem = () => {
+const CartItem = ({name, price} : { name: string, price: number}) => {
   const { addToCart, removeFromCart }: any = useCart();
   const [quantity, setQuantity] = useState(1);
   return (
@@ -15,8 +15,8 @@ const CartItem = () => {
         <Image source={wine_png} style={styles.image} />
       </View>
       <View style={styles.textContainer}>
-        <Text style={styles.label}>{"label"}</Text>
-        <Text style={styles.price}>{"price"}</Text>
+        <Text style={styles.label}>{name}</Text>
+        <Text style={styles.price}>{price}</Text>
       </View>
       <View style={styles.counterContainer}>
         <Pressable onPress={addToCart}>
@@ -36,10 +36,11 @@ export default function Cart({ navigation, route }: any) {
   const { products, addToCart, removeFromCart, clearCart }: any = useCart();
   return (
     <View style={styles.container}>
-      {products && products.length > 0 ? (
-      <CartItem />
-      ) : (
-        <Text style={styles.emptyText}>{"Nenhum Produto"}</Text>
+      {products && products.length > 0 && products.map((product: any) => {
+        <CartItem name={product.name} price={product.price} />
+      })}
+      {products.length === 0 && (
+        <Text style={styles.emptyText}>{"Nenhum Produto Adicionado"}</Text>
       )}
     </View>
   );
